78d6770eb9fa015b3b82104409e74423ea7c488c
Isaia Tonini <isaia.tonini@gmail.com>
2024-01-21 00:02:00 +0100

    modified auth test main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import AuthPanel from '../components/AuthPanel.svelte';
+</script>
+
+<section class="text-primary-600">
+	<h1>Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+	<AuthPanel />
+</section>
+
