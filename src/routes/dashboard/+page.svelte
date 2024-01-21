c49278aa04adebd56159aa0493567e57a7358007
Isaia Tonini <isaia.tonini@gmail.com>
2024-01-22 00:07:19 +0100

    added logout button and image to dashboard (testing)

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,13 +1,23 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
+	import { handleSignOut, user } from '../../store/store';
+	import { getAuth, onAuthStateChanged } from 'firebase/auth';
+	const auth = getAuth();
 
-	export let data;
-
-	onMount(()=>{
-		console.log(data);
+	onAuthStateChanged(auth, (newUser) => {
+	  $user = newUser;
 	});
-	
 </script>
 
 <h1 class="text-primary-600">DASHBOARD</h1>
 <p>Questa e' la dashboard</p>
+
+{#if $user !== null}
+	<div class="flex items-center gap-4">
+	{#if $user.photoURL}
+		<img src={$user.photoURL} alt="Profile" style="width: 50px; height: 50px; border-radius: 50%;">
+	{/if}
+	<p>Hi {$user.displayName}</p>
+	</div>
+	<button on:click={handleSignOut}>LOGOUT</button>
+{/if}
