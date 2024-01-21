0658b952a2c0e9859f5b0e4eac812902c922229e
Isaia Tonini <isaia.tonini@gmail.com>
2024-01-21 21:55:28 +0100

    fixed return object from query

--- src/routes/dashboard/+page.server.ts
+++ src/routes/dashboard/+page.server.ts
@@ -1,5 +1,5 @@
 import { db } from "$lib/firebase/firebase";
-import { collection, query,/*  where, */ getDocs } from "firebase/firestore";
+import { collection, query,  getDocs } from "firebase/firestore";
 
 export async function load() {
 	const q = query(collection(db, "tickets"));
@@ -7,15 +7,15 @@ export async function load() {
 	const querySnapshot = await getDocs(q);
 	
 	const tickets = querySnapshot.docs.map(doc => {
-
-        return ({
-        id: doc.id,
-		ticketID: doc.data().ticketID,
-        name: doc.data().name,
-        surname: doc.data().surname,
-        checkIn: doc.data().checkIn.toDate()
-
-    })});
+        return (
+			{
+				id: doc.id,
+				name: doc.data().name,
+				surname: doc.data().surname,
+				checkIn: doc.data().checkIn ? doc.data().checkIn.toDate() : null,
+			}
+		);
+	});
 
 	return {
 		data: {
