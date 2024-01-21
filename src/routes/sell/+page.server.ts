4db485254dd1848c29d6cca903de6122c8523243
Isaia Tonini <isaia.tonini@gmail.com>
2024-01-21 13:05:44 +0100

    fixed database entry creation with right format

--- src/routes/sell/+page.server.ts
+++ src/routes/sell/+page.server.ts
@@ -1,25 +1,20 @@
 import { db } from "$lib/firebase/firebase";
 import type { Actions } from "./$types";
-import { /* collection, */ doc, setDoc, Timestamp } from "firebase/firestore"; 
+import { /* collection, */ doc, setDoc, /* Timestamp */ } from "firebase/firestore"; 
 
 
 export const actions: Actions = {
 	default: async ({ request/* , locals */ }) => {
 		const formData = await request.formData();
+		
 		const name = formData.get("name");
 		const surname = formData.get("surname");
 		const code = formData.get("code");
-		console.log(name);
-		console.log(surname);
-		console.log(code);
 
-		//const ticketsTable = collection(db, "tickets");
-
-		await setDoc(doc(db, "tickets", "Test"), {
-			ticketID: code,
+		await setDoc(doc(db, "tickets", `${code}`), {
 			name: name,
 			surname: surname,
-			checkIn: Timestamp.fromDate(new Date()),
+			checkIn: null
 		});
 	}
 };
