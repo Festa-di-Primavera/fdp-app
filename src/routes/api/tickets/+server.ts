e594838af8d9d6d45a9489d6112a9ab28d9b3802
Isaia Tonini <isaia.tonini@gmail.com>
2024-01-23 23:36:03 +0100

    changed return tyoe

--- src/routes/api/tickets/+server.ts
+++ src/routes/api/tickets/+server.ts
@@ -1,20 +1,23 @@
 import { clientDB } from '$lib/firebase/firebase.js';
 import { json } from '@sveltejs/kit';
 import { collection, getDocs, query } from 'firebase/firestore';
+import type { Ticket } from '../../../models/ticket';
 
 export async function GET() {
 	const q = query(collection(clientDB, "tickets"));
 
 	const querySnapshot = await getDocs(q);
 	
-	const tickets = querySnapshot.docs.map(doc => {
+	const tickets = querySnapshot.docs.map(ticketDoc => {
         return (
 			{
-				id: doc.id,
-				name: doc.data().name,
-				surname: doc.data().surname,
-				checkIn: doc.data().checkIn ? doc.data().checkIn.toDate() : null,
-			}
+				ticketID: ticketDoc.id,
+				name: ticketDoc.data().name,
+				surname: ticketDoc.data().surname,
+				checkIn: ticketDoc.data().checkIn ? ticketDoc.data().checkIn.toDate().toLocaleString('it-IT', { timeZone: 'Europe/Rome' }) : null,
+				soldAt: ticketDoc.data().soldAt ? ticketDoc.data().soldAt.toDate().toLocaleString('it-IT', { timeZone: 'Europe/Rome' }) : null,
+				seller: ticketDoc.data().seller
+			} as Ticket
 		);
 	});
 
