d3b877de84843ebd67939441ca3b599a2c5b9848
Isaia Tonini <isaia.tonini@gmail.com>
2024-01-25 11:23:00 +0100

    added accessLevel to customclaims

--- src/routes/api/role/[uid]/[newRole]/+server.ts
+++ src/routes/api/role/[uid]/[newRole]/+server.ts
@@ -1,11 +1,20 @@
 import { initAdmin } from '$lib/firebase/firebaseAdmin';
 import { json } from '@sveltejs/kit';
+import { roles } from '../../../../../models/role';
 
 export async function PUT({params}) {
 	const app = initAdmin();
+	
+	const enumBindings: {[key: string]: roles} = {
+		'normal': roles.NORMAL,
+		'seller': roles.SELLER,
+		'check-in': roles.CHECKIN,
+		'admin': roles.ADMIN,
+		'superadmin': roles.SUPERADMIN
+	};
 
 	try{
-		app.auth().setCustomUserClaims(params.uid, { role: params.newRole });
+		app.auth().setCustomUserClaims(params.uid, {...(await app.auth().getUser(params.uid)).customClaims, role: params.newRole, accessLevel: enumBindings[params.newRole]});
 		return json({
 			status: 200,
 			body: {
