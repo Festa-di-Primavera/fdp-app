4e387ce85b7bf56b148d5e7619fd935116fcfe10
Riccardo Benevelli <rickybenevelli@gmail.com>
2024-01-26 17:48:28 +0100

    fix(models): fix type of ticket model

--- src/models/ticket.ts
+++ src/models/ticket.ts
@@ -1,8 +1,10 @@
+import type { Timestamp } from "firebase-admin/firestore"
+
 export type Ticket = {
 	ticketID: string,
 	name: string,
 	surname: string,
-	checkIn: string,
-	soldAt: string,
+	checkIn: Timestamp,
+	soldAt: Timestamp,
 	seller: string
 }
