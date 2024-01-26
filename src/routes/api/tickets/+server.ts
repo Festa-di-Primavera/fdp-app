adc173d1ebee8eea4b5d66072528c541d771aa3f
Isaia Tonini <isaia.tonini@gmail.com>
2024-01-26 14:15:58 +0100

    changed seller field to user's alias

--- src/routes/api/tickets/+server.ts
+++ src/routes/api/tickets/+server.ts
@@ -2,21 +2,30 @@ import { clientDB } from '$lib/firebase/firebase.js';
 import { json } from '@sveltejs/kit';
 import { collection, getDocs, query, setDoc, doc } from 'firebase/firestore';
 import type { Ticket } from '../../../models/ticket';
+import { initAdmin } from '$lib/firebase/firebaseAdmin';
+import { roles } from '../../../models/role';
 
 export async function GET() {
 	const q = query(collection(clientDB, "tickets"));
 
+	const app = initAdmin();
+	const users = await app.auth().listUsers();
+	const sellers = users.users.filter((user) => user.customClaims?.accessLevel >= roles.SELLER);
+
+
 	const querySnapshot = await getDocs(q);
+
 	
-	const tickets = querySnapshot.docs.map(ticketDoc => {
-        return (
+	
+	const tickets = querySnapshot.docs.map((ticketDoc) => {
+    	return (
 			{
 				ticketID: ticketDoc.id,
 				name: ticketDoc.data().name,
 				surname: ticketDoc.data().surname,
 				checkIn: ticketDoc.data().checkIn ? ticketDoc.data().checkIn.toDate().toLocaleString('it-IT', { timeZone: 'Europe/Rome' }) : null,
 				soldAt: ticketDoc.data().soldAt ? ticketDoc.data().soldAt.toDate().toLocaleString('it-IT', { timeZone: 'Europe/Rome' }) : null,
-				seller: ticketDoc.data().seller
+				seller: ticketDoc.data().seller ? sellers.find((seller) => seller.uid === ticketDoc.data().seller)?.customClaims?.alias : null
 			} as Ticket
 		);
 	});
