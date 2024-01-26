6006954c955db6ca2fb630a4b170bebf1c996827
Isaia Tonini <isaia.tonini@gmail.com>
2024-01-26 22:43:51 +0100

    changed import type

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,5 +1,5 @@
-const tailwindcss = require('tailwindcss');
-const autoprefixer = require('autoprefixer');
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 const config = {
 	plugins: [
