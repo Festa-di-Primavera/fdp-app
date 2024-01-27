94b7f1bf93c417f0f2aafeed9fee71d33fdd2a52
Isaia Tonini <isaia.tonini@gmail.com>
2024-01-27 08:43:13 +0100

    rechanged import to require (vercel fail)

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,5 +1,5 @@
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
 
 const config = {
 	plugins: [
