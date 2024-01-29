d8b0a642863d2fc48c16dbbdb6d0833dd4c40cb7
Riccardo Benevelli <rickybenevelli@gmail.com>
2024-01-29 23:45:05 +0100

    modify check-in bar chart algorithm

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -15,46 +15,79 @@
 	export let data: { strTicketData: string };
 
 	let tickets: Ticket[] = JSON.parse(data.strTicketData) as Ticket[];
-	
-	let numberOfCheckIns: number = 0;
-	let ticketsCheckIn: { x: string, y: number }[] = [];
 
-	let checkedTicketsCount: number = tickets.filter(ticket => ticket.checkIn !== null).length;
-	let notCheckedTicketsCount: number = tickets.filter(ticket => ticket.soldAt !== null).length - checkedTicketsCount;
-	let notSoldTicketsCount: number = tickets.filter(ticket => ticket.soldAt === null).length;
+	let numberOfCheckIns: number = 0;
+	let ticketsCheckIn: { x: string; y: number }[] = [];
+
+	let checkedTicketsCount: number = tickets.filter((ticket) => ticket.checkIn !== null).length;
+	let notCheckedTicketsCount: number =
+		tickets.filter((ticket) => ticket.soldAt !== null).length - checkedTicketsCount;
+	let notSoldTicketsCount: number = tickets.filter((ticket) => ticket.soldAt === null).length;
+
+	interface TimeSlotData {
+		x: string;
+		y: number;
+	}
 
 	let mappings: Map<string, number> = new Map();
-	function computeData(tickets: Ticket[]): void {
-		const mapHourToCount: Map<string, number> = new Map();
-		
-		for(let ticket of tickets){
-			if(ticket.checkIn !== null){
-				const date = new Date(ticket.checkIn);
-				const hour = date.getHours();
-				const count = mapHourToCount.get(hour.toString()) || 0;
-				mapHourToCount.set(hour.toString(), count + 1);
-				numberOfCheckIns++;
+
+	function computeData(tickets: Ticket[], slotTime: number, maxSlots: number): TimeSlotData[] {
+		const timeSlotsMap: Map<number, number> = new Map();
+
+		for (let ticket of tickets) {
+			if (ticket.checkIn !== null) {
+				const timestamp = new Date(ticket.checkIn).getTime();
+				const slotIndex = Math.floor(timestamp / slotTime);
+				timeSlotsMap.set(slotIndex, (timeSlotsMap.get(slotIndex) || 0) + 1);
+				numberOfCheckIns++; // TODO: qui conto il numero totale di checkin, mentre dovrei considerare solo quelli all'interno dello slot
 			}
 
-			if(ticket.seller !== null){
+			if (ticket.seller !== null) {
 				mappings.set(ticket.seller, (mappings.get(ticket.seller) || 0) + 1);
 			}
 		}
-		
-		mapHourToCount.forEach((value, key) => {
-			ticketsCheckIn.push({ x: key, y: value });
-		});
-		ticketsCheckIn.sort((a, b) => parseInt(a.x) - parseInt(b.x));
-		ticketsCheckIn = ticketsCheckIn;
-	}
-	
-	computeData(tickets);
 
-	onMount(async() => {
+		const sortedTimeSlots = Array.from(timeSlotsMap.entries()).sort((a, b) => a[0] - b[0]);
+
+		const timeSlotData: TimeSlotData[] = [];
+		let currentSlotIndex = sortedTimeSlots[0][0];
+		let currentIndex = 0;
+
+		while (currentIndex < sortedTimeSlots.length && timeSlotData.length < maxSlots) {
+			if (sortedTimeSlots[currentIndex][0] === currentSlotIndex) {
+				const timestamp = currentSlotIndex * slotTime;
+				const date = new Date(timestamp);
+
+				const hours = date.getHours().toString().padStart(2, '0');
+				const minutes = date.getMinutes().toString().padStart(2, '0');
+
+				const label = `${hours}:${minutes}`; // Formatta l'ora come "hh:mm"
+				timeSlotData.push({ x: label, y: sortedTimeSlots[currentIndex][1] });
+				currentIndex++;
+			} else {
+				const timestamp = currentSlotIndex * slotTime;
+				const date = new Date(timestamp);
+
+				const hours = date.getHours().toString().padStart(2, '0');
+				const minutes = date.getMinutes().toString().padStart(2, '0');
+
+				const label = `${hours}:${minutes}`;
+				timeSlotData.push({ x: label, y: 0 });
+			}
+			currentSlotIndex++;
+		}
+
+		return timeSlotData; //.reverse();
+	}
+
+	ticketsCheckIn = computeData(tickets, 1000 * 60 * 60, 100);
+	console.log(ticketsCheckIn);
+
+	onMount(async () => {
 		onAuthStateChanged(getAuth(getClientApp()), (newUser) => {
 			$user = newUser;
-			if($user === null){
-				goto("/");
+			if ($user === null) {
+				goto('/');
 				return;
 			}
 		});
@@ -62,31 +95,37 @@
 </script>
 
 {#if $user}
-	<section class="w-full h-full flex flex-col items-center gap-4">
-		<div class="w-full px-5 pt-5 flex flex-col gap-4 items-start max-w-96 pb-12">
-			<h1 class="text-primary-600 font-bold text-4xl">Dashboard</h1>
-			<p class="dark:text-white text-justify">Informazioni relative ai biglietti</p>
-			
-			<div class="w-full grid grid-cols-2 gap-2 grid-flow-row-dense">
-				<Card class="col-span-2 w-full flex flex-col gap-5 items-center pt-6">
-					<h1 class="text-primary-600 font-bold text-5xl text-center">{checkedTicketsCount !== undefined ? checkedTicketsCount : '--'}</h1>
-					<p class="dark:text-white text-center">Biglietti validati</p>
+	<section class="flex h-full w-full flex-col items-center gap-4">
+		<div class="flex w-full max-w-96 flex-col items-start gap-4 px-5 pb-12 pt-5">
+			<h1 class="text-4xl font-bold text-primary-600">Dashboard</h1>
+			<p class="text-justify dark:text-white">Informazioni relative ai biglietti</p>
+
+			<div class="grid w-full grid-flow-row-dense grid-cols-2 gap-2">
+				<Card class="col-span-2 flex w-full flex-col items-center gap-5 pt-6">
+					<h1 class="text-center text-5xl font-bold text-primary-600">
+						{checkedTicketsCount !== undefined ? checkedTicketsCount : '--'}
+					</h1>
+					<p class="text-center dark:text-white">Biglietti validati</p>
 				</Card>
 
-				<Card class="w-full aspect-square flex flex-col gap-5 items-center pt-6">
-					<h1 class="text-primary-600 font-bold text-5xl text-center">{notCheckedTicketsCount !== undefined ? notCheckedTicketsCount : '--'}</h1>
-					<p class="dark:text-white text-center">Biglietti venduti non validati</p>
+				<Card class="flex aspect-square w-full flex-col items-center gap-5 pt-6">
+					<h1 class="text-center text-5xl font-bold text-primary-600">
+						{notCheckedTicketsCount !== undefined ? notCheckedTicketsCount : '--'}
+					</h1>
+					<p class="text-center dark:text-white">Biglietti venduti non validati</p>
 				</Card>
 
-				<Card class="w-full aspect-square flex flex-col gap-5 items-center pt-6">
-					<h1 class="text-primary-600 font-bold text-5xl text-center">{notSoldTicketsCount !== undefined ? notSoldTicketsCount : '--'}</h1>
-					<p class="dark:text-white text-center">Biglietti non venduti</p>
+				<Card class="flex aspect-square w-full flex-col items-center gap-5 pt-6">
+					<h1 class="text-center text-5xl font-bold text-primary-600">
+						{notSoldTicketsCount !== undefined ? notSoldTicketsCount : '--'}
+					</h1>
+					<p class="text-center dark:text-white">Biglietti non venduti</p>
 				</Card>
 			</div>
 
-			<Tickets bind:checkedTicketsCount bind:notSoldTicketsCount bind:notCheckedTicketsCount/>
-			<TicketsPerPerson bind:mappings/>
-			<CheckInPerTime bind:ticketsCheckIn bind:numberOfCheckIns/>
+			<Tickets bind:checkedTicketsCount bind:notSoldTicketsCount bind:notCheckedTicketsCount />
+			<TicketsPerPerson bind:mappings />
+			<CheckInPerTime bind:ticketsCheckIn bind:numberOfCheckIns />
 		</div>
 	</section>
 {/if}
