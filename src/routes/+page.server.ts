7c6c5e7e000c027e6862b928d84f9aeb81afb7b8
Isaia Tonini <isaia.tonini@gmail.com>
2024-02-04 22:20:00 +0100

    added type safety

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,11 +1,11 @@
-import { getAuth } from 'firebase-admin/auth';
+import { getAuth, type DecodedIdToken } from 'firebase-admin/auth';
 
 import { getAdminApp, getClaimsFromIdToken } from '$lib/firebase/admin';
 
 export async function load({cookies}) {
 	const app = getAuth(getAdminApp());
 
-	const user = await getClaimsFromIdToken(cookies);
+	const user: DecodedIdToken | null = await getClaimsFromIdToken(cookies);
 
 	if (user) {
 		const tok = await app.createCustomToken(user?.uid || '');
