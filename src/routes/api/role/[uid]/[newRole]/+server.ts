afa7f2230f3dd8c5e82a49b4f83d1ca9cdda91fc
Isaia Tonini <isaia.tonini@gmail.com>
2024-02-05 10:06:54 +0100

    added error handling

--- src/routes/api/role/[uid]/[newRole]/+server.ts
+++ src/routes/api/role/[uid]/[newRole]/+server.ts
@@ -1,7 +1,7 @@
 import { getAdminApp } from '$lib/firebase/admin';
 import { getAuth } from 'firebase-admin/auth';
-import { json } from '@sveltejs/kit';
 import { roles } from '../../../../../models/role';
+import type { FirebaseError } from 'firebase-admin/app';
 
 export async function PUT({params}) {
 	const app = getAuth(getAdminApp());
@@ -15,20 +15,38 @@ export async function PUT({params}) {
 	};
 
 	try{
-		app.setCustomUserClaims(params.uid, {...(await app.getUser(params.uid)).customClaims, role: params.newRole, accessLevel: enumBindings[params.newRole]}); // TODO: valutare se modificare la stringa e utilizzare il numero
-		return json({
-			status: 200,
-			body: {
-				message: 'User role updated'
-			}
-		});
+		await app.setCustomUserClaims(params.uid, {...(await app.getUser(params.uid)).customClaims, role: params.newRole, accessLevel: enumBindings[params.newRole]});
+		
+		const resp = new Response(
+			JSON.stringify({message: 'Ruolo modificato con successo'}),
+			{status: 200, headers: {'Content-Type': 'application/json'}}
+		);
+		
+		console.log(`Ruolo modificato con successo: ${params.uid} -> ${params.newRole}`);
+		return resp;
 	}
 	catch(e){
-		return json({
-			status: 500,
-			body: {
-				message: (e as Error).message
-			}
-		});
+		let message = 'Errore sconosciuto';
+		switch ((e as FirebaseError).code) {
+			case 'firebase-auth/user-not-found':
+			  message = 'Utente non trovato';
+			  break;
+			case 'firebase-auth/permission-denied':
+			  message = 'Permesso negato';
+			  break;
+			case 'app/network-error':
+			  message = 'Errore di rete!!!';
+			  break;
+			default:
+			  message = 'Errore sconosciuto';
+			  break;
+		}
+		
+		const resp = new Response(
+			JSON.stringify({message: message}),
+			{status: 500, headers: {'Content-Type': 'application/json'}}
+		);
+
+		return resp;
 	}
 }
