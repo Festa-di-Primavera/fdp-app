e0e03e30d0a5abf1e200fe750d5adbe0133b354b
Isaia Tonini <isaia.tonini@gmail.com>
2024-02-12 08:26:55 +0100

    temporary protected "generate" page access

--- src/routes/generate/+page.server.ts
+++ src/routes/generate/+page.server.ts
@@ -2,14 +2,15 @@ import { getAuth } from 'firebase-admin/auth';
 
 import { getAdminApp, getClaimsFromIdToken } from '$lib/firebase/admin';
 
-import { roles } from '../../models/role';
+/* import { roles } from '../../models/role'; */
 import { redirect } from '@sveltejs/kit';
 
 export async function load({cookies}) {
 	const app = getAuth(getAdminApp());
 	const user = await getClaimsFromIdToken(cookies);
 
-	if (user?.accessLevel >= roles.SUPERADMIN) {
+	/* if (user?.accessLevel >= roles.SUPERADMIN) { */
+	if(user?.email === import.meta.env.VITE_ADMIN_EMAIL1 || user?.email === import.meta.env.VITE_ADMIN_EMAIL2) {
 		const tok = await app.createCustomToken(user?.uid || '');
 
 		return {
