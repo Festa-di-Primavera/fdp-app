6c85db0eb24607a0c5334bc40261162b962f1b0d
Isaia Tonini <isaia.tonini@gmail.com>
2024-02-15 20:09:09 +0100

    moved toasts to top

--- src/components/UsersTable.svelte
+++ src/components/UsersTable.svelte
@@ -207,7 +207,7 @@
 	</div>
 {/if}
 
-<Toast on:close={() => toastOpen = false} bind:open={toastOpen} color={color} class="w-max mt-5 mx-auto right-0 left-0 fixed bottom-5" divClass= 'w-full max-w-xs p-2 text-gray-500 bg-white shadow dark:text-gray-400 dark:bg-gray-700 gap-3'>
+<Toast on:close={() => toastOpen = false} bind:open={toastOpen} color={color} class="w-max mt-5 mx-auto right-0 left-0 fixed top-20" divClass= 'w-full max-w-xs p-2 text-gray-500 bg-white shadow dark:text-gray-400 dark:bg-gray-700 gap-3'>
 	<svelte:component this={error ? XCircle : CheckCircle2} class="w-6 h-6  text-{color}-400" slot="icon"/>
 	<span class={`text-${color}-400 font-semibold`}>{message}</span>
 </Toast>
--- src/routes/generate/+page.svelte
+++ src/routes/generate/+page.svelte
@@ -235,7 +235,7 @@
 	{/if}
 </Toast>
 
-<Toast on:close={() => toastOpen = false} bind:open={toastOpen} color="red" class="w-max mt-10 mb-5 mx-auto right-0 left-0 fixed bottom-5" divClass= 'w-full max-w-xs p-2 text-gray-500 bg-white shadow dark:text-gray-400 dark:bg-gray-700 gap-3'>
+<Toast on:close={() => toastOpen = false} bind:open={toastOpen} color="red" class="w-max mt-10 mb-5 mx-auto right-0 left-0 fixed top-20" divClass= 'w-full max-w-xs p-2 text-gray-500 bg-white shadow dark:text-gray-400 dark:bg-gray-700 gap-3'>
 	<XCircle class="w-6 h-6  text-red-400" slot="icon"/>
 	<span class='text-red-400 font-semibold'>{toastMessage}</span>
 </Toast>
