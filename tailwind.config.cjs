50aa5a73e90c29402361389e288cc90fb13f8f39
Isaia Tonini <isaia.tonini@gmail.com>
2024-02-24 17:22:55 +0100

    changed default color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,18 +8,17 @@ const config = {
 	theme: {
 	  extend: {
 		colors: {
-		  // flowbite-svelte
-		  primary: {
-			50: '#FFF5F2',
-			100: '#FFF1EE',
-			200: '#FFE4DE',
-			300: '#FFD5CC',
-			400: '#FFBCAD',
-			500: '#FE795D',
-			600: '#EF562F',
-			700: '#EB4F27',
-			800: '#CC4522',
-			900: '#A5371B'
+		  primary:{
+			50:'#E6A1E9',
+			100: '#E089E3',
+			200: '#D972DE',
+			300: '#D35AD8',
+			400: '#CD42D3',
+			500: '#C72BCD',
+			600: '#C114C8',
+			700: '#AD12B4',
+			800: '#9A10A0',
+			900: '#870E8C',
 		  }
 		}
 	  }
