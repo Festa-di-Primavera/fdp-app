c495729e4b3f92d82abd7ddf8804714fcf177cf3
Isaia Tonini <isaia.tonini@gmail.com>
2024-03-01 15:29:36 +0100

    moved handleSignOut function

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,23 +1,6 @@
-//import type { ListUsersResult } from "firebase-admin/auth";
-import { goto } from '$app/navigation';
-import { getAuth, signOut, type User } from 'firebase/auth';
+import { type User } from 'firebase/auth';
 import { writable, type Writable } from 'svelte/store';
 
 export const user: Writable<User | null> = writable(null);
-
-export const handleSignOut = async () => {
-	try {
-		await signOut(getAuth());
-		await fetch('/api/session', {
-			method: 'DELETE'
-		});
-
-		user.set(null);
-
-		goto('/');
-	} catch (error) {
-		console.error(error);
-	}
-};
 
 export const theme: Writable<'light' | 'dark'> = writable();
