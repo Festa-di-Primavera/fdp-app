2c2978e32344b3781776da7e2bdacd1c349fd70e
Isaia Tonini <isaia.tonini@gmail.com>
2024-03-18 23:23:32 +0100

    completely changed qr scanner

--- src/components/QrReader.svelte
+++ src/components/QrReader.svelte
@@ -1,23 +1,12 @@
 <script lang="ts">
-    import jsQR, { type QRCode } from 'jsqr';
+    import QrScanner from 'qr-scanner';
     import { onDestroy, onMount } from 'svelte';
     import { CameraIcon, X } from 'lucide-svelte';
     import { Button, Dropdown, DropdownHeader, DropdownItem } from 'flowbite-svelte';
-
-	export let videoWidth: number = 900;
-	export let videoHeight: number = 900;
-	export let borderColor: string = "#1ac814";
     export let codeResult: string = '';
 
-	let aspectClass = `aspect-${videoWidth}/${videoHeight}`;
-
-    let video: HTMLVideoElement;
-    let canvasElement: HTMLCanvasElement | null;
-    let canvas: CanvasRenderingContext2D | null;
-    let stream: MediaStream | null;
-
-    let imageData: ImageData;
-    let code: QRCode | null;
+    let videoFrame: HTMLVideoElement;
+    let qrScanner: QrScanner;
 
     let alreadyFound = false;
     let opened = false;
@@ -28,15 +17,39 @@
     let devices: MediaDeviceInfo[] = [];
     let selectedCam: string;
 
-    onMount(() => {
-        video = document.createElement("video");
-        canvasElement = document.getElementById("canvas") as HTMLCanvasElement;
-        canvas = canvasElement!.getContext("2d", { willReadFrequently: true });
-        drawReaderIcon(canvasElement!, canvas!);
-        navigator.mediaDevices.enumerateDevices().then(devs => {
-            devices = devs.filter(device => device.kind === 'videoinput');
-            selectedCam = devices[0].deviceId;
-        });
+    onMount(async () => {
+        try{
+            const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+            stream.getTracks().forEach(track => track.stop());
+        }
+        catch (error) {
+            /* 
+                TODO: handle error 
+                message: Device in use
+            */
+        }
+        
+        const devicesList = await navigator.mediaDevices.enumerateDevices();
+        devices = devicesList.filter(device => device.kind === 'videoinput');
+
+        qrScanner = new QrScanner(
+            videoFrame,
+            result => {codeResult = result.data; pauseScannerOnCode();},
+            {
+                onDecodeError: error => {
+                    codeResult = '';
+                },
+                highlightCodeOutline: true,
+                highlightScanRegion: true,
+                //overlay: document.getElementById('overlay') as HTMLDivElement
+            }
+        );
+
+        let index = devices.findIndex(device => device.label.toLowerCase().includes('back'));
+        index = index === -1 ? 0 : index;
+
+        updateCamera(devices[index].deviceId);
+
         document.addEventListener("visibilitychange", handleVisibilityChange);
     });
 
@@ -51,173 +64,90 @@
         }   
     }
 
-    async function updateVideoStream() {
-        if (stream) {
-            stream.getTracks().forEach(track => track.stop());
-        }
-        stream = await navigator.mediaDevices.getUserMedia({ video: { deviceId: selectedCam } });
-        video.srcObject = stream;
-        video.play();
+    async function updateCamera(cam: string) {
+        selectedCam = cam;
+        qrScanner.setCamera(cam);
+        camSelectOpen = false;
     }
 
     async function openScanner(){
-        if(!opened || code){
-            opened = true;
-            alreadyFound = false;
-            codeResult = '';
-            code = null;
+        if(!opened) {
+            try{
+                await qrScanner.start();
+                opened = true;
+                alreadyFound = false;
+                codeResult = '';
+            } catch (error) {
+                /* 
+                    TODO: handle error
+                    error === 'Camera not found.' <- it is a string
+                */
+            }
 
-            video = document.createElement("video");
-            canvasElement = document.getElementById("canvas") as HTMLCanvasElement;
-            canvas = canvasElement!.getContext("2d", { willReadFrequently: true });
-
-            updateVideoStream();
-
-            requestAnimationFrame(tick);
+            return
         }
-        isPaused = false;
+
+
+        if(isPaused){
+            try{
+                await qrScanner.$video.play();
+                isPaused = false;
+                codeResult = '';
+                alreadyFound = false;
+            } catch (error) {
+                /* 
+                    TODO: handle error
+                    error === 'Camera not found.' <- it is a string
+                */
+            }
+
+            isPaused = false;
+            return;
+        }
+
     }
 
     function pauseScannerOnCode() {
-        if (code) {
-            video!.pause();
-            video!.srcObject = null;
-            video!.src = "";
-            video!.remove();
-            isPaused = true;
-        }
-    }
-
-    function drawReaderIcon(canvasElement: HTMLCanvasElement, canvas: CanvasRenderingContext2D){
-        let svgString = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 122.61 122.88">
-                            <defs>
-                                <style>
-                                .cls-1 {
-                                    fill-rule: evenodd;
-                                }
-                                </style>
-                            </defs>
-                            <path fill="white" class="cls-1" d="M26.68,26.77H51.91V51.89H26.68V26.77ZM35.67,0H23.07A22.72,22.72,0,0,0,14.3,1.75a23.13,23.13,0,0,0-7.49,5l0,0a23.16,23.16,0,0,0-5,7.49A22.77,22.77,0,0,0,0,23.07V38.64H10.23V23.07a12.9,12.9,0,0,1,1-4.9A12.71,12.71,0,0,1,14,14l0,0a12.83,12.83,0,0,1,9.07-3.75h12.6V0ZM99.54,0H91.31V10.23h8.23a12.94,12.94,0,0,1,4.9,1A13.16,13.16,0,0,1,108.61,14l.35.36h0a13.07,13.07,0,0,1,2.45,3.82,12.67,12.67,0,0,1,1,4.89V38.64h10.23V23.07a22.95,22.95,0,0,0-6.42-15.93h0l-.37-.37a23.16,23.16,0,0,0-7.49-5A22.77,22.77,0,0,0,99.54,0Zm23.07,99.81V82.52H112.38V99.81a12.67,12.67,0,0,1-1,4.89,13.08,13.08,0,0,1-2.8,4.17,12.8,12.8,0,0,1-9.06,3.78H91.31v10.23h8.23a23,23,0,0,0,16.29-6.78,23.34,23.34,0,0,0,5-7.49,23,23,0,0,0,1.75-8.8ZM23.07,122.88h12.6V112.65H23.07A12.8,12.8,0,0,1,14,108.87l-.26-.24a12.83,12.83,0,0,1-2.61-4.08,12.7,12.7,0,0,1-.91-4.74V82.52H0V99.81a22.64,22.64,0,0,0,1.67,8.57,22.86,22.86,0,0,0,4.79,7.38l.31.35a23.2,23.2,0,0,0,7.5,5,22.84,22.84,0,0,0,8.8,1.75Zm66.52-33.1H96v6.33H89.59V89.78Zm-12.36,0h6.44v6H70.8V83.47H77V77.22h6.34V64.76H89.8v6.12h6.12v6.33H89.8v6.33H77.23v6.23ZM58.14,77.12h6.23V70.79h-6V64.46h6V58.13H58.24v6.33H51.8V58.13h6.33V39.33h6.43V58.12h6.23v6.33h6.13V58.12h6.43v6.33H77.23v6.33H70.8V83.24H64.57V95.81H58.14V77.12Zm31.35-19h6.43v6.33H89.49V58.12Zm-50.24,0h6.43v6.33H39.25V58.12Zm-12.57,0h6.43v6.33H26.68V58.12ZM58.14,26.77h6.43V33.1H58.14V26.77ZM26.58,70.88H51.8V96H26.58V70.88ZM32.71,77h13V89.91h-13V77Zm38-50.22H95.92V51.89H70.7V26.77Zm6.13,6.1h13V45.79h-13V32.87Zm-44,0h13V45.79h-13V32.87Z" />
-                        </svg>`;
-        
-        let svgUrl = "data:image/svg+xml;base64," + btoa(svgString);
-
-        const img = new Image();
-        img.src = svgUrl;
-        let minSize = Math.min(canvasElement!.width, canvasElement!.height);
-        img.width = img.height = minSize/2;
-        img.onload = function() {
-            let textHeight = minSize/10;
-            let text = "Scansiona";
-
-            canvas!.drawImage(img, canvasElement!.width/2 - img.width/2, canvasElement!.height/2 - img.height/2 - textHeight/2, img.width, img.height);
-            canvas!.font = textHeight + "px Arial";
-            canvas!.fillStyle = "white";
-            canvas!.fillText(text, canvasElement!.width/2 - canvas!.measureText(text).width/2, canvasElement!.height/2 - img.height/2 - textHeight/2 + img.height + 1.5*textHeight);
-        };
+        qrScanner.$video.pause();
+        isPaused = true;
 
     }
 
     function closeScanner() {
-		if (opened) {
-			video!.remove();
-			
-            stream!.getTracks().forEach(function(track) {
-				track.stop();
-            });
-			canvas?.clearRect(0, 0, canvasElement!.width, canvasElement!.height);
-            drawReaderIcon(canvasElement!, canvas!);
+		if (opened) {			
+            qrScanner.stop();
 
 			opened = false;
-            isPaused = true;
         }
-    }
-
-    function drawLine(begin: any, end: any, borderColor: string, xOffset: number, yOffset: number) {
-		canvas?.beginPath();
-		canvas?.moveTo(begin.x + xOffset, begin.y + yOffset);
-		canvas?.lineTo(end.x + xOffset, end.y + yOffset);
-		canvas!.lineWidth = 7;
-		canvas!.strokeStyle = borderColor;
-		canvas?.stroke();
-	}
-
-    function tick() {
-        if (video.readyState === video.HAVE_ENOUGH_DATA) {
-            // Calcola le dimensioni e la posizione per centrare e tagliare il video sulla canvas
-            const aspectRatio = video.videoWidth / video.videoHeight;
-            const canvasAspectRatio = canvasElement!.width / canvasElement!.height;
-            let drawWidth, drawHeight, xOffset, yOffset;
-
-            if (aspectRatio < canvasAspectRatio) {
-                drawWidth = canvasElement!.width;
-                drawHeight = drawWidth / aspectRatio;
-                xOffset = 0;
-                yOffset = (canvasElement!.height - drawHeight) / 2;
-            } else {
-                drawHeight = canvasElement!.height;
-                drawWidth = drawHeight * aspectRatio;
-                yOffset = 0;
-                xOffset = (canvasElement!.width - drawWidth) / 2;
-            }
-
-            // Disegna il video sulla canvas
-            canvas!.drawImage(video, xOffset, yOffset, drawWidth, drawHeight);
-
-            // Usa la porzione visibile del video per la rilevazione del codice QR
-            imageData = canvas!.getImageData(xOffset, yOffset, drawWidth, drawHeight);
-            code = jsQR(imageData.data, imageData.width, imageData.height, {
-                inversionAttempts: "dontInvert",
-            });
-
-            if (code) {
-                drawLine(code.location.topLeftCorner, code.location.topRightCorner, borderColor, xOffset, yOffset);
-				drawLine(code.location.topRightCorner, code.location.bottomRightCorner, borderColor, xOffset, yOffset);
-				drawLine(code.location.bottomRightCorner, code.location.bottomLeftCorner, borderColor, xOffset, yOffset);
-				drawLine(code.location.bottomLeftCorner, code.location.topLeftCorner, borderColor, xOffset, yOffset);
-
-                alreadyFound = true;
-                codeResult = code.data;
-                pauseScannerOnCode();
-            }
-        }
-
-        if (opened) {
-            if(!alreadyFound)
-                requestAnimationFrame(tick);
-        }
-		else {
-			canvas?.clearRect(0, 0, canvasElement!.width, canvasElement!.height);
-			drawReaderIcon(canvasElement!, canvas!);
-		}
     }
 </script>
 
 <div class="flex flex-col gap-3 items-center">
-    <div class="relative w-[80%] md:w-96 rounded-xl border-primary-600 border-4 dark:bg-gray-600 bg-gray-400">
-        <Button class="absolute top-2 left-2 aspect-square rounded-md bg-transparent dark:bg-transparent dark:hover:bg-opacity-30 hover:bg-opacity-30 focus-within:ring-0" on:click={()=>{camSelectOpen = !camSelectOpen}}>
-            <CameraIcon class="absolute w-6 h-6 dark:text-primary-300 text-primary-800 z-50" />
+    <div class="relative w-[80%] md:w-96 aspect-square rounded-xl border-primary-600 border-4 dark:bg-gray-600 bg-gray-400" id="videocontainer">
+        <Button class="absolute top-2 left-2 aspect-square rounded-md bg-transparent dark:bg-transparent dark:hover:bg-opacity-30 hover:bg-opacity-30 focus-within:ring-0" id="camSelector">
+            <CameraIcon class="absolute w-6 h-6 dark:text-primary-800 text-primary-800 z-[25]" />
         </Button>
-        {#if devices.length > 0}
-            <Dropdown bind:open={camSelectOpen} placement="bottom-start">
-                <DropdownHeader>
-                    Fotocamere
-                </DropdownHeader>
-                {#each devices as device}
-                    <DropdownItem on:click={()=>{selectedCam = device.deviceId; if(!isPaused){ updateVideoStream();} camSelectOpen = false}}>
-                        {device.label}
-                    </DropdownItem>
-                {/each}
-            </Dropdown>
-        {/if}
-        <canvas on:click={openScanner} id="canvas" class="w-full rounded-lg {aspectClass}" width={videoWidth} height={videoHeight}>
-        </canvas>
+        <!-- svelte-ignore a11y-media-has-caption -->
+        <video on:click={openScanner} class="w-full h-full object-cover rounded-lg " bind:this={videoFrame}/>
+        <div id="overlay" class="border-2 rounded-xl border-primary-600" />
         {#if opened}
-        <button type="button" class="flex items-center gap-1 text-primary-400 absolute top-2 right-2 z-10 bg-slate-400 bg-opacity-40 p-1 rounded-md" on:click={closeScanner}>
-            Chiudi
-            <X class="w-5 h-5 text-primary-400" />
-        </button>
+            <button type="button" class="flex items-center gap-1 text-primary-400 dark:text-primary-900 absolute top-2 right-2 z-10 bg-slate-400 bg-opacity-40 p-1 rounded-md" on:click={closeScanner}>
+                Chiudi
+                <X class="w-5 h-5 text-primary-400 dark:text-primary-900" />
+            </button>
         {/if}
     </div>
-    <span class="text-primary-500 text-center font-semibold">{isPaused ? 'Clicca nel riquadro per scansionare' : 'Scansione in corso'}</span>
+    <span class="text-primary-500 text-center font-semibold">{(!opened || isPaused) ? 'Clicca nel riquadro per scansionare' : 'Scansione in corso'}</span>
 </div>
+{#if devices.length > 0}
+    <Dropdown bind:open={camSelectOpen} placement="bottom-start" triggeredBy="#camSelector">
+        <DropdownHeader>
+            Fotocamere
+        </DropdownHeader>
+        {#each devices as device}
+            <DropdownItem on:click={()=>{updateCamera(device.deviceId);}} class={device.deviceId === selectedCam ? "dark:text-primary-200 text-primary-800" : ''}>
+                {device.label}
+            </DropdownItem>
+        {/each}
+    </Dropdown>
+{/if}
