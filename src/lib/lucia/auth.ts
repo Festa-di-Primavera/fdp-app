04b752175ae3c73d6846c1d8b291f029c4a5e86a
Isaia Tonini <isaia.tonini@gmail.com>
2024-08-18 09:56:47 +0200

    fixed redirect uri

--- src/lib/lucia/auth.ts
+++ src/lib/lucia/auth.ts
@@ -8,7 +8,9 @@ import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from "$env/static/private";
 import { getClientDB } from "$lib/firebase/client";
 import { FirestoreAdapter } from "./firestore-adapter";
 
-export const google = new Google(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, "http://localhost:5173/api/login/google/callback");
+const redirectUri = dev ?  "http://localhost:5173/api/login/google/callback" : "https://fdp-app.vercel.app/api/login/google/callback";
+
+export const google = new Google(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, redirectUri);
 export const googleCodeVerifier = generateCodeVerifier();
 
 dotenv.config({ path: `${resolve()}/.env` });
