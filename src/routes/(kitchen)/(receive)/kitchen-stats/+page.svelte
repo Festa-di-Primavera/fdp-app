ea2207b955cfaa99a0c076076595fc2271dbc5b0
Leonardo Segala <lallo.segala@gmail.com>
2026-04-22 19:35:56 +0200

    Add counter

--- src/routes/(kitchen)/(receive)/kitchen-stats/+page.svelte
+++ src/routes/(kitchen)/(receive)/kitchen-stats/+page.svelte
@@ -3,15 +3,18 @@
     import { ORDERS } from "$lib/firebase/collections";
     import { type Order, ItemType } from "$models/order";
     import {
+        AlertTriangle,
         Beef,
         Hamburger,
         Leaf,
         PawPrint,
         Sandwich,
+        TriangleAlert,
         UtensilsCrossed,
         WheatOff,
     } from "@lucide/svelte";
     import {
+        type Timestamp,
         type Unsubscribe,
         onSnapshot,
         query,
@@ -21,7 +24,9 @@
 
     let unsubscribe: Unsubscribe = () => {};
     let unsubscribeCompleted: Unsubscribe = () => {}; // New unsubscribe for completed orders
+    let unsubscribeAll: Unsubscribe = () => {}; // New unsubscribe for all orders
 
+    let totalOrders: Order[] = $state([]);
     let inProgressOrders: Order[] = $state([]);
     let completedOrders: Order[] = $state([]); // New state for completed orders
 
@@ -56,15 +61,15 @@
         }, 0),
     );
 
-    const glutenFreeCount = $derived(
-        inProgressOrders.reduce((count, order) => {
-            // Count gluten-free items in the order
-            const glutenFreeItems = order.items
-                .filter((item) => item.glutenFree === true)
-                .reduce((total, item) => total + item.quantity, 0);
-            return count + glutenFreeItems;
-        }, 0),
-    );
+    // const glutenFreeCount = $derived(
+    //     inProgressOrders.reduce((count, order) => {
+    //         // Count gluten-free items in the order
+    //         const glutenFreeItems = order.items
+    //             .filter((item) => item.glutenFree === true)
+    //             .reduce((total, item) => total + item.quantity, 0);
+    //         return count + glutenFreeItems;
+    //     }, 0),
+    // );
 
     const hotdogCount = $derived(
         inProgressOrders.reduce((count, order) => {
@@ -76,46 +81,6 @@
         }, 0),
     );
 
-    // Stats for completed orders - total sandwiches and hamburgers
-    const totalSandwichesConsumed = $derived(
-        completedOrders.reduce((count, order) => {
-            // Sum all order items regardless of type
-            const sandwichesInOrder = order.items
-                .filter(
-                    (item) =>
-                        item.type === ItemType.BASIC ||
-                        item.type === ItemType.ONTO,
-                )
-                .reduce((total, item) => total + item.quantity, 0);
-            return count + sandwichesInOrder;
-        }, 0),
-    );
-
-    const totalHamburgersConsumed = $derived(
-        completedOrders.reduce((count, order) => {
-            // Sum only BASIC and ONTO items (which contain meat)
-            const hamburgersInOrder = order.items
-                .filter(
-                    (item) =>
-                        item.type === ItemType.BASIC ||
-                        item.type === ItemType.ONTO,
-                )
-                .reduce((total, item) => total + item.quantity, 0);
-            return count + hamburgersInOrder;
-        }, 0),
-    );
-
-    const totalWurstelConsumed = $derived(
-        completedOrders.reduce((count, order) => {
-            return (
-                count +
-                order.items
-                    .filter((item) => item.type === ItemType.HOTDOG)
-                    .reduce((total, item) => total + item.quantity, 0)
-            );
-        }, 0),
-    );
-
     // get orders from firestore - modified to fetch both in-progress and completed orders
     function getOrders() {
         // Query for in-progress orders
@@ -123,6 +88,14 @@
 
         // Query for completed orders
         const qCompleted = query(ORDERS, where("done", "==", true));
+
+        unsubscribeAll = onSnapshot(query(ORDERS), (querySnapshot) => {
+            totalOrders = querySnapshot.docs.map((doc) => ({
+                ...doc.data(),
+                firebaseId: doc.id,
+                creationDate: (doc.data().creationDate as Timestamp).toDate(),
+            })) as Order[];
+        });
 
         // Subscribe to in-progress orders with optimized handling
         unsubscribe = onSnapshot(qInProgress, (querySnapshot) => {
@@ -209,74 +182,204 @@
     onDestroy(() => {
         unsubscribe();
         unsubscribeCompleted(); // Unsubscribe from completed orders query
+        unsubscribeAll();
     });
+
+    function countItems(orders: Order[], items: ItemType[]) {
+        let tot = 0;
+
+        for (const order of orders) {
+            for (const item of order.items) {
+                if (items.includes(item.type)) {
+                    tot += item.quantity;
+                }
+            }
+        }
+
+        return tot;
+    }
+
+    const MAX_BEEF_AVAILABLE = 395;
+    const MAX_BREAD_AVAILABLE = 445;
+    const MAX_HOTDOG_AVAILABLE = 295;
+
+    const MAX_GLUTENFREE_HAMBURGERS = 12;
+    const MAX_GLUTENFREE_HOTDOGS = 12;
+
+    const totalBeefConsumed = $derived(
+        countItems(totalOrders, [ItemType.BASIC, ItemType.ONTO]),
+    );
+
+    const totalBreadConsumed = $derived(
+        countItems(totalOrders, [
+            ItemType.BASIC,
+            ItemType.ONTO,
+            ItemType.VEGETARIANO,
+        ]),
+    );
+
+    const totalHotdogConsumed = $derived(
+        countItems(totalOrders, [ItemType.HOTDOG]),
+    );
 </script>
 
 <svelte:head>
     <title>Statistiche Cucina</title>
+    <style>
+        @keyframes alert {
+            0% {
+                scale: 100%;
+            }
+            25% {
+                scale: 120%;
+            }
+            50% {
+                scale: 100%;
+            }
+            75% {
+                scale: 80%;
+            }
+            100% {
+                scale: 100%;
+            }
+        }
+        .animate-alert {
+            animation: alert 1s infinite linear;
+        }
+    </style>
 </svelte:head>
 
 <div class="px-4 w-full flex flex-col">
-    <!-- <p>SENZA GLUTINE: {glutenFreeCount}</p> -->
-
-    <!-- <p class="my-5">=== FATTI ===</p>
-    <p>VEGETARIANI: {0}</p>
-    <p>ONTI: {0}</p>
-    <p>BASIC: {0}</p>
-    <p>SENZA GLUTINE: {0}</p>
-    <p>HOT DOG: {0}</p> -->
+    {countItems(totalOrders, [ItemType.HOTDOG])}
 
     <!-- Card con statistiche di consumo -->
     <div class="grow flex flex-wrap gap-10 justify-evenly items-center mt-16">
-        <Card.Root class="w-96 p-14">
-            <Card.Content class="p-0">
-                <div class="flex flex-col items-center justify-center gap-3">
-                    <Beef class="w-10 h-10 text-red-500" />
-                    <span
-                        class="text-5xl font-bold text-red-600 dark:text-red-400"
-                    >
-                        {totalHamburgersConsumed}
-                    </span>
-                    <span
-                        class="text-xl text-center font-medium dark:text-white"
-                    >
-                        Hamburger consumati
-                    </span>
-                </div>
+        <Card.Root class="w-96 p-14 relative">
+            <Card.Content
+                class="p-0 flex flex-col items-center justify-center gap-3"
+            >
+                <Beef class="w-10 h-10 text-red-500" />
+                <span class="text-5xl font-bold text-red-600 dark:text-red-400">
+                    {totalBeefConsumed} / {MAX_BEEF_AVAILABLE}
+                </span>
+                <span class="text-xl text-center font-medium dark:text-white">
+                    Lucanica consumata
+                </span>
             </Card.Content>
+            {#if totalBeefConsumed >= MAX_BEEF_AVAILABLE - 20}
+                <TriangleAlert
+                    class="absolute top-5 right-5 text-red-600 animate-alert"
+                    size="30px"
+                />
+            {/if}
         </Card.Root>
-        <Card.Root class="w-96 p-14">
-            <Card.Content class="p-0">
-                <div class="flex flex-col items-center justify-center gap-3">
-                    <Hamburger class="w-10 h-10 text-app-accent" />
-                    <span class="text-5xl font-bold text-app-accent/80">
-                        {totalSandwichesConsumed}
-                    </span>
-                    <span
-                        class="text-xl text-center font-medium dark:text-white"
-                    >
-                        Pane consumato
-                    </span>
-                </div>
+        <Card.Root class="w-96 p-14 relative">
+            <Card.Content
+                class="p-0 flex flex-col items-center justify-center gap-3"
+            >
+                <Hamburger class="w-10 h-10 text-app-accent" />
+                <span class="text-5xl font-bold text-app-accent/80">
+                    {totalBreadConsumed} / {MAX_BREAD_AVAILABLE}
+                </span>
+                <span class="text-xl text-center font-medium dark:text-white">
+                    Pane consumato
+                </span>
             </Card.Content>
+            {#if totalBreadConsumed >= MAX_BREAD_AVAILABLE - 20}
+                <TriangleAlert
+                    class="absolute top-5 right-5 text-red-600 animate-alert"
+                    size="30px"
+                />
+            {/if}
         </Card.Root>
-        <Card.Root class="w-96 p-14">
-            <Card.Content class="p-0">
-                <div class="flex flex-col items-center justify-center gap-3">
-                    <Beef class="w-10 h-10 text-red-500" />
-                    <span
-                        class="text-5xl font-bold text-red-600 dark:text-red-400"
-                    >
-                        {totalWurstelConsumed}
-                    </span>
-                    <span
-                        class="text-xl text-center font-medium dark:text-white"
-                    >
-                        Hotdog consumati
-                    </span>
-                </div>
+        <Card.Root class="w-96 p-14 relative">
+            <Card.Content
+                class="p-0 flex flex-col items-center justify-center gap-3"
+            >
+                <Beef class="w-10 h-10 text-red-500" />
+                <span class="text-5xl font-bold text-red-600 dark:text-red-400">
+                    {totalHotdogConsumed} / {MAX_HOTDOG_AVAILABLE}
+                </span>
+                <span class="text-xl text-center font-medium dark:text-white">
+                    Hotdog consumati
+                </span>
             </Card.Content>
+            {#if totalHotdogConsumed >= MAX_HOTDOG_AVAILABLE - 20}
+                <TriangleAlert
+                    class="absolute top-5 right-5 text-red-600 animate-alert"
+                    size="30px"
+                />
+            {/if}
         </Card.Root>
+
+        <!-- <Card.Root class="w-96 p-14">
+            <Card.Content class="p-0 h-full flex items-center justify-center">
+                <div class="flex flex-col items-center justify-center gap-6">
+                    <div
+                        class="flex flex-col items-center justify-center gap-3"
+                    >
+                        <Beef class="w-10 h-10 text-red-500" />
+                        <span
+                            class="text-5xl font-bold text-red-600 dark:text-red-400"
+                        >
+                            {totalHotdogConsumed} / {MAX_HOTDOG_AVAILABLE}
+                        </span>
+                        <span
+                            class="text-xl text-center font-medium dark:text-white"
+                        >
+                            Senza glutine consumati
+                        </span>
+                    </div> -->
+
+        <!-- <span
+                        class="text-6xl font-bold text-violet-600 dark:text-violet-400"
+                    >
+                        {basicCount + ontiCount}
+                    </span>
+                    <span
+                        class="text-3xl text-center font-medium dark:text-white"
+                    >
+                        Non-vegetariani in coda
+                    </span> -->
+
+        <!-- <div class="flex gap-6 mt-6 w-full">
+                        <Card.Root class="flex-1 p-14">
+                            <Card.Content class="p-0">
+                                <div class="flex flex-col items-center gap-4">
+                                    <span
+                                        class="text-5xl font-bold text-orange-600 dark:text-orange-400"
+                                    >
+                                        {ontiCount}
+                                    </span>
+                                    <span
+                                        class="text-2xl text-center font-medium text-orange-600 dark:text-orange-400"
+                                    >
+                                        Onti
+                                    </span>
+                                </div>
+                            </Card.Content>
+                        </Card.Root>
+
+                        <Card.Root class="flex-1 p-14">
+                            <Card.Content class="p-0">
+                                <div class="flex flex-col items-center gap-4">
+                                    <span
+                                        class="text-5xl font-bold text-blue-600 dark:text-blue-400"
+                                    >
+                                        {basicCount}
+                                    </span>
+                                    <span
+                                        class="text-2xl text-center font-medium text-blue-600 dark:text-blue-400"
+                                    >
+                                        Basic
+                                    </span>
+                                </div>
+                            </Card.Content>
+                        </Card.Root>
+                    </div> -->
+        <!-- </div>
+            </Card.Content>
+        </Card.Root> -->
     </div>
 
     <hr class="my-15" />
